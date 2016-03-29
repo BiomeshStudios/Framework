5b592c801714b60984f503d0ad8eca74ff7ae70a
Biomesh-Repo <relser65@biomeshstudios.com>
2016-03-29 13:50:23 +0100

    Initialised

---
+++ Framework/Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  Framework.h
+//  Framework
+//
+//  Created by Rob Elser on 29/03/2016.
+//  Copyright © 2016 Biomesh Studios, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
+
+
